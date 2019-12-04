a4951f09d1f391b9e540acb00befd1b43cd6c737
ann99marm <43759995+ann99marm@users.noreply.github.com>
2019-12-04 20:57:19 +0200

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -1,20 +1,297 @@
-import java.util.ArrayList;
+package dmst.mebede.group12.vrp;
 
-public class Truck {
-	public int space = 3000;
-	public int v = 35;
-	public ArrayList route= new ArrayList();
-	public int truckId;
-	public Truck(int truckId) {
-		this.truckId = truckId;
-	}
-	public boolean addToTruck(int demand) {
-		boolean placement = true;
-		if ((this.space-demand) >= 0) {
-			this.space = this.space - demand;
-		}else {
-			placement = false;
-		}
-		return placement;
-	}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+
+public class VehicleRoutingProblem {
+    protected static AtomicInteger truckCounter = new AtomicInteger(0);
+    protected static AtomicInteger nodeCounter = new AtomicInteger(0);
+    private int totalTrucks, totalServicePoints;
+    private Node depository;
+    private ArrayList<Node> allNodes;
+    private ArrayList<Truck> allTrucks;
+    private ArrayList<Route> routes;
+    private double[][] distanceMatrix, timeMatrix;
+
+    public VehicleRoutingProblem(int totalServicePoints, int totalTrucks) {
+        this.totalServicePoints = totalServicePoints;
+        this.totalTrucks = totalTrucks;
+        generateRandomNetwork();
+        createTrucks();
+        initializeRoutes();
+        convertDistanceToTime(distanceMatrix);
+        printMatrix(distanceMatrix);
+        applyAdvanced();
+        printFinalData();
+    }
+
+    private void printFinalData() {
+        for(Route r : routes) {
+            System.out.println("Route " + r.getRouteID() + ": ");
+            System.out.println(r.getRouteNodes().toString());
+            System.out.print("Route duration in hours: ");
+            System.out.printf("%.2f", r.getTotalRouteTimeInHrs());
+            System.out.println("\n");
+        }
+
+        Route maxRoute = Collections.max(routes, Comparator.comparing(r -> r.getTotalRouteTimeInHrs()));
+        System.out.println("Longest route: " + maxRoute.getRouteID() + " (" + maxRoute.getTotalRouteTimeInHrs() + " hours)");
+
+    }
+
+    private void generateRandomNetwork() {
+        createAllNodesAndServicePointLists();
+        calculateEuclideanDistanceMatrix();
+    }
+
+    private void createAllNodesAndServicePointLists() {
+        allNodes = new ArrayList<Node>();
+
+        depository = new Node(50 , 50 , 0);
+        allNodes.add(depository);
+
+        for (int i = 0; i < totalServicePoints; i++) {
+            Node customer = new Node();
+            allNodes.add(customer);
+        }
+    }
+    
+  /*  public void clarkWright() {
+    	double[][] save = new double[distanceMatrix[0].length][distanceMatrix[1].length];
+    	for (int i = 0;i <= allNodes.size(); i++) {
+    		for (int j = 0; j <= allNodes.size(); j++) {
+    			save[i][j] = timeMatrix[i][0] + timeMatrix[0][j] - timeMatrix[i][j];
+    		}
+    	}
+    	
+    }*/
+    
+    private void calculateEuclideanDistanceMatrix() {
+        this.distanceMatrix = new double[allNodes.size()][allNodes.size()];
+        for (int i = 0; i < allNodes.size(); i++) {
+            Node from = allNodes.get(i);
+
+            for (int j = 0; j < allNodes.size(); j++) {
+
+                Node to = allNodes.get(j);
+
+                double deltaX = (from.getX() - to.getX());
+                double deltaY = (from.getY() - to.getY());
+                double distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+
+                distance = Math.round(distance);
+                distanceMatrix[i][j] = distance;
+            }
+        }
+    }
+
+    private void createTrucks() {
+        allTrucks = new ArrayList<Truck>();
+        for(int i = 0; i < totalTrucks; i++) {
+            allTrucks.add(new Truck(depository));
+        }
+    }
+
+    private void initializeRoutes() {
+        routes = new ArrayList<Route>(totalTrucks);
+
+        for(Truck truck : allTrucks) {
+            //create an initial Route for each truck
+            Route route = new Route(truck);
+            route.addNodeToRoute(depository);
+            routes.add(route);
+        }
+    }
+
+
+    private void printMatrix(double[][] matrix) {
+        System.out.print("\t");
+        int i = 0;
+        do {
+            System.out.print(" " + i + "\t");
+            i++;
+        } while (i < allNodes.size());
+        System.out.println();
+        System.out.print("\t");
+        for(i = 0; i < allNodes.size(); i++) {
+            if (i != allNodes.size() - 1) {
+                System.out.print("________");
+            } else {
+                System.out.print("____");
+            }
+        }
+        System.out.println();
+        for (i = 0; i < allNodes.size(); i++) {
+            System.out.print(i + "\t" + "|");
+            for (int j = 0; j < allNodes.size(); j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    private void applyAdvanced() {
+        for (int i = 0; i < allNodes.size()-1; i++) {
+            ArrayList<Solution> potentialNodes;
+            ArrayList<Route> nQuickestRoutes;
+            do {
+                nQuickestRoutes = findNQuickestRoutes(4);
+                
+                potentialNodes = new ArrayList<>(nQuickestRoutes.size());
+                for (Route r : nQuickestRoutes) {
+                	System.out.println(r.toString());
+                    potentialNodes.add(findMinUnservicedPoint(r));
+                }
+                System.out.println(potentialNodes.toString());
+            } while(potentialNodes.contains(null));
+            Pair<Route, Node> winningPair = findBestAddition(nQuickestRoutes, potentialNodes);
+            Route minScoreRoute = winningPair.getRoute();
+            Node nodeToInsert = winningPair.getNode();
+            System.out.println(winningPair.toString());
+            minScoreRoute.addNodeToRoute(nodeToInsert, winningPair.getPositionInRoute());
+            nodeToInsert.updateServiceStatus(true);
+            int truckCap = minScoreRoute.getTruck().getRemainingCap();
+            minScoreRoute.getTruck().setRemainingCap(truckCap - nodeToInsert.getDemand());
+            minScoreRoute.setTotalRouteTimeInHrs(minScoreRoute.getTotalRouteTimeInHrs() + 
+            				timeMatrix[minScoreRoute.getRouteNodes().get(winningPair.getPositionInRoute()).getNodeID()][nodeToInsert.getNodeID()] 
+            							+ timeMatrix[nodeToInsert.getNodeID()][minScoreRoute.getRouteNodes().get(winningPair.getPositionInRoute()).getNodeID() + 1] + 0.25);
+        }
+    }
+
+    private Pair<Route, Node> findBestAddition(ArrayList<Route> nQuickestRoutes, ArrayList<Solution> potentialSolution) {
+        //List<Node> currentNodes = nQuickestRoutes.stream().map(Route::getTruck).map(Truck::getCurrentNode).collect(Collectors.toList());
+
+    	Route minScoreRoute = null;
+        Node nodeToInsert  = null;
+        Solution s = null;
+        double min = Double.MAX_VALUE;
+        for(int j = 0; j < nQuickestRoutes.size(); j++) {
+            Route r = nQuickestRoutes.get(j);
+            double score = r.getTotalRouteTimeInHrs() + potentialSolution.get(j).getCost();
+            if (score < min) {
+                min = score;
+                s =  potentialSolution.get(j);
+                minScoreRoute = r;
+                nodeToInsert  = potentialSolution.get(j).getNode();
+            }
+        }
+        return new Pair<Route, Node>(minScoreRoute, nodeToInsert, s.getPos());
+    }
+    
+    private  Solution findMinUnservicedPoint(Route currentQuickestRoute) {
+    	Truck truck = currentQuickestRoute.getTruck();
+        double min = Double.MAX_VALUE;
+        int solutionInsertionPoint = -1;
+        int minIndex = -1;
+        double totalCost = Double.MAX_VALUE;
+        System.out.println("Inside " +currentQuickestRoute.getRouteNodes().size());
+        if (currentQuickestRoute.getRouteNodes().size() == 1) {
+        	for(int j = 1; j < allNodes.size(); j++) {
+        		System.out.println("Inside " + currentQuickestRoute.getRouteNodes().size());
+        		Node candidate = allNodes.get(j);
+        		if (!candidate.isServiced()) {
+        			if (candidate.getDemand() <= truck.getRemainingCap()) {
+        				Node a = currentQuickestRoute.getRouteNodes().get(0);
+        				double trialCost = timeMatrix[a.getNodeID()][j];
+        				if(trialCost != 0D && trialCost < min) {
+        					min = trialCost;
+        					totalCost = timeMatrix[a.getNodeID()][j];
+        					minIndex = j;
+        					solutionInsertionPoint = 0;
+        				}
+        			}
+        		}
+        	}
+        } else {
+        	for (int i = 0; i < currentQuickestRoute.getRouteNodes().size() - 1; i++) {
+        		for(int j = 1; j < allNodes.size(); j++) {
+        			System.out.println("Inside " + currentQuickestRoute.getRouteNodes().size());
+        			Node candidate = allNodes.get(j);
+        			if (!candidate.isServiced()) {
+        				Node a = currentQuickestRoute.getRouteNodes().get(i);
+        				Node b = currentQuickestRoute.getRouteNodes().get(i + 1);
+        				if (!(a.isServiced()) && !(b.isServiced()) && candidate.getDemand() <= truck.getRemainingCap()) {
+        					double trialCost = timeMatrix[a.getNodeID()][j] + timeMatrix[j][b.getNodeID()]- timeMatrix[a.getNodeID()][b.getNodeID()];
+        					if(trialCost != 0D && trialCost < min) {
+        						min = trialCost;
+        						totalCost = timeMatrix[a.getNodeID()][j] + timeMatrix[j][b.getNodeID()];
+        						minIndex = j;
+        						solutionInsertionPoint = i;
+        					}
+        				}
+        			}
+        		}
+        	}
+        }
+        if (minIndex != -1) {
+        	Solution s = new Solution(allNodes.get(minIndex), solutionInsertionPoint, totalCost, min);
+            return s;
+        } else {
+            currentQuickestRoute.setFinalised(true);
+            return null;
+        }
+    }
+
+    private ArrayList<Route> findNQuickestRoutes(int n) {
+        List<Route> availableRoutes = routes.stream().filter(t -> !t.isFinalised()).collect(Collectors.toList());
+        List<Route> sortedList = availableRoutes.stream().sorted(Comparator.comparingDouble(Route::getTotalRouteTimeInHrs)).collect(Collectors.toList());
+        if (sortedList.size() >= n) {
+            return new ArrayList<Route>(sortedList.subList(0, n));
+        } else {
+            return new ArrayList<Route>(sortedList);
+        }
+    }
+
+    private void convertDistanceToTime(double[][] distanceMatrix) {
+        int numberOfRows = distanceMatrix[0].length;
+        int numberOfCols = distanceMatrix[1].length;
+        this.timeMatrix = new double[numberOfRows][numberOfCols];
+
+        for(int row = 0; row < numberOfRows; row++) {
+            for (int col = 0; col < numberOfCols; col++) {
+                timeMatrix[row][col] = distanceMatrix[row][col] / Truck.speedKMH;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        new VehicleRoutingProblem(200, 25);
+        long end = System.currentTimeMillis();
+        float duration = (end - start) / 1000F;
+        System.out.println();
+        System.out.print("Algorithm execution duration in seconds: ");
+        System.out.printf("%.3f", duration);
+    }
+
+    public int getTotalTrucks() {
+        return totalTrucks;
+    }
+
+    public void setTotalTrucks(int totalTrucks) {
+        this.totalTrucks = totalTrucks;
+    }
+
+    public int getTotalServicePoints() {
+        return totalServicePoints;
+    }
+
+    public void setTotalServicePoints(int totalServicePoints) {
+        this.totalServicePoints = totalServicePoints;
+    }
+
+    public ArrayList<Node> getAllNodes() {
+        return allNodes;
+    }
+
+    public void setAllNodes(ArrayList<Node> allNodes) {
+        this.allNodes = allNodes;
+    }
+
 }
